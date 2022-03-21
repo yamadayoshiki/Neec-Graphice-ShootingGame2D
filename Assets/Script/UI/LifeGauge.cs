a39015de52fb5004f4fec8ab97bd2cf1e49b1ff8
yoshiki <raikinex430@gmail.com>
2022-03-21 17:56:55 +0900

    Boss登場時にボスの体力ゲージを表示するように実装

--- Assets/Script/UI/LifeGauge.cs
+++ Assets/Script/UI/LifeGauge.cs
@@ -10,7 +10,16 @@ public class LifeGauge : MonoBehaviour
 	/// ‘Ï‹v’lƒNƒ‰ƒX
 	/// </summary>
 	[SerializeField]
-	public Life Life = null;
+	private Life m_Life = null;
+	public Life Life
+	{
+		get { return m_Life; }
+		set
+		{
+			m_Life = value;
+			m_GaugeObjects.ForEach((obj) => obj.SetActive(true));
+		}
+	}
 
 	/// <summary>
 	/// ƒQ[ƒW‰æ‘œ
@@ -18,8 +27,20 @@ public class LifeGauge : MonoBehaviour
 	[SerializeField]
 	private Image m_GaugeImage = null;
 
+	[SerializeField]
+	private List<GameObject> m_GaugeObjects = new List<GameObject>();
+
+	private void Start()
+	{
+		if (m_Life == null)
+		{
+			m_GaugeObjects.ForEach((obj) => obj.SetActive(false));
+		}
+	}
+
 	private void Update()
 	{
+		if (Life == null) return;
 		//‰æ‘œ‚É‘Ï‹v’l‚ÌŠ„‡‚ğ“n‚·
 		m_GaugeImage.fillAmount = Life.LifeRate;
 	}
